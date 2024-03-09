94df43da66b0069a406b4aeaa33e7b4a94c204d0
Robert <rommulaner@googlemail.com>
2024-03-09 21:21:47 +0100

    Update trinamic.cpp
    added code to use slower baud rate for extruder to be able to compile working T/TE version. Otherwise printer goes in boot loop.

--- Marlin/src/module/stepper/trinamic.cpp
+++ Marlin/src/module/stepper/trinamic.cpp
@@ -612,7 +612,12 @@ enum StealthIndex : uint8_t {
       #ifdef E0_HARDWARE_SERIAL
         HW_SERIAL_BEGIN(E0);
       #else
-        stepperE0.beginSerial(TMC_BAUD_RATE);
+        //stepperE0.beginSerial(TMC_BAUD_RATE);   related to issue #22 to be able to compile T/TE version with UART
+        #ifdef TMC_E0_BAUD_RATE
+          stepperE0.beginSerial(TMC_E0_BAUD_RATE); // use slower baud rate if defined
+        #else
+          stepperE0.beginSerial(TMC_BAUD_RATE);   // else use standard rate
+        #endif
       #endif
     #endif
     #if AXIS_HAS_UART(E1)
