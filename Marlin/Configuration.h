c29384c890c62996a90be432bdd01f22cbb8cfe1
Jonathan Vetter <Virtual.Java@mail.de>
2024-04-09 15:00:59 +0200

    Try to fix offsets2 fails XMin to high (>0) throws error 'Movement bounds (Y_MIN_POS, Y_MAX_POS) are too narrow to contain Y_BED_SIZE.'

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1768,7 +1768,7 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS -20.0
-#define Y_MIN_POS 10
+#define Y_MIN_POS -0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
