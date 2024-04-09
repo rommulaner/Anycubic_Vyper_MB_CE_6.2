586acd54ad5f184ba0dff178975cae65a71e0fd2
Jonathan Vetter <Virtual.Java@mail.de>
2024-04-09 14:56:56 +0200

    Try to fix offsets2 for nozzle, printing bed size, X_Min_Switches and Y_Min_Switches

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1767,8 +1767,8 @@
 #define Y_BED_SIZE 230
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -11.0
-#define Y_MIN_POS -21.4
+#define X_MIN_POS -20.0
+#define Y_MIN_POS 10
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
